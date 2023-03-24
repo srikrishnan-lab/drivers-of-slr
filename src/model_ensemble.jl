a6c098ca890816dbaf5647957058baeaeb868ff9
darnellchl <ced227@cornell.edu>
2023-03-24 17:24:59 -0400

    add statistical noise and GtC conversion

--- src/model_ensemble.jl
+++ src/model_ensemble.jl
@@ -4,6 +4,7 @@ Pkg.activate(joinpath(@__DIR__, "..")) # this does the same thing: Pkg.activate(
 Pkg.instantiate()
 
 # import necessary packages
+using Random
 using Mimi
 using MimiSNEASY
 using MimiBRICK
@@ -12,47 +13,35 @@ using Impute
 using Dates
 using Distributions
 using DataStructures
+using XLSX
 using CSVFiles # need to keep for "load" function
-
-import GlobalSensitivityAnalysis
-gsa = GlobalSensitivityAnalysis # shorthand name
 
 include("functions.jl") # include functions from other scripts
 
-"""
-x = final_co2_df.Emissions
-rv1 = Mimi.EmpiricalDistribution(x)
-rand(rv1) # gives one value
-
-# write function so that normal random samples are positive
-function pos_normal(; μ,σ)
-    x = rand(Normal(μ,σ))
-    return (x ≥ 0) ? x : pos_normal(μ,σ)
-end
-
-# define the data for DICE
-data = gsa.SobolData(
-    params = OrderedDict(:cca0 => Uniform(80, 100),
-                         :k0   => Uniform(125, 145)),
-                         :sneasy_brick_param_1 => Uniform(0,1),
-                         :sneasy_brick_param_2 => Uniform(0,1),
-    N = 2 # set to 1024)
-"""
-
 # initial set up
-start_year = 2010
+Random.seed!(1)
+output_dir = "default"
+num_samples = 1000
+start_year = 1850
 end_year = 2300
 model_years = collect(start_year:end_year)
 num_years = length(model_years)
-num_samples = 1000
 num_params = 38
 
-# store dataframe with info for each RCP scenario for appropriate years
+# store dataframe with info for each RCP scenario for appropriate years (historical data from beginning-2005)
 RCP26 = scenario_df("RCP26")[in(model_years).(scenario_df("RCP26").year), :]
 RCP45 = scenario_df("RCP45")[in(model_years).(scenario_df("RCP45").year), :]
 RCP60 = scenario_df("RCP60")[in(model_years).(scenario_df("RCP60").year), :]
 RCP85 = scenario_df("RCP85")[in(model_years).(scenario_df("RCP85").year), :]
-# alternative way to subset: RCP85 = scenario_df("RCP85")[start_year .<= scenario_df("RCP85").year .<= end_year, :]
+
+# add in more recent historical observations (2006-2021)
+recent_data = DataFrame(XLSX.readtable(joinpath(@__DIR__, "..", "data", "Global_Carbon_Budget_2022v1.0.xlsx"), "Global Carbon Budget", first_row=21)) # read in data
+recent_data = recent_data[2006 .<= recent_data.Year .<= 2021, [1,2,3]] # get emissions from years 2006-2021
+recent_data = DataFrame(year=recent_data.Year, rcp_co2_emissions=(recent_data[:,2] .+ recent_data[:,3]) .* 3.67) # add fossil emissions + land-use change emissions, and convert to units of GtCO₂
+
+# combine older and recent historical data
+older_data = RCP60[start_year .<= RCP60.year .<= 2005, [:year, :rcp_co2_emissions]] # get historical data from 1850-2005 (same for all RCP scenarios)
+historical_data = vcat(older_data, recent_data) # older_data is 1850-2005; recent_data is 2006-2021
 
 # create instance of SNEASY-BRICK
 m = MimiBRICK.create_sneasy_brick(start_year=start_year, end_year=end_year) # 1 year timestep
@@ -60,18 +49,13 @@ m = MimiBRICK.create_sneasy_brick(start_year=start_year, end_year=end_year) # 1 
 #explore(m)
 
 # generate num_samples of parameter samples for emissions curves
-growth  = rand(Uniform(0.001,0.012), num_samples)              # growth parameter      (original = 0.0089), rand(Uniform(0,0.02))
-peak    = trunc.(Int64, rand(Uniform(2030,2200), num_samples)) # peaking time          (original = 2115), rand(Uniform(2030,2200))
-decline = rand(Uniform(0.005,0.13), num_samples)               # decline parameter     rand(Uniform(0.005,0.126))
+growth  = rand(truncated(Normal(0.004,0.0075), 0.001, Inf), num_samples)           # growth parameter
+peak    = trunc.(Int64, rand(truncated(Normal(2070,25), 2030, 2200), num_samples)) # peaking time
+decline = rand(truncated(Normal(0.07,0.05), 0.001, 0.2), num_samples)              # decline parameter
+# add in sampling for land water storage
+lw_storage =  rand(Normal(0.0003, 0.00018), num_samples, num_years)
 # read in ensemble of parameter samples for SNEASY-BRICK
 calibrated_params = DataFrame(load(joinpath(@__DIR__, "..", "data", "calibrated_parameters", "parameters_subsample_sneasybrick.csv"))) # read in subsample, not full chain
-
-"""# generate samples using Sobol sequence
-samples = gsa.sample(data) # Saltelli sampler generates N*(2D+2) samples, where N is number of runs and D is number of params
-
-# get number of samples and years
-num_samples = 1:size(samples,1)
-num_years = length(collect(2010:2305))"""
 
 # pre-allocate arrays to store results
 parameters                  = zeros(Float64, num_samples, num_params)
@@ -79,11 +63,12 @@ co2_emissions               = zeros(Float64, num_samples, num_years)
 radiative_forcing           = zeros(Float64, num_samples, num_years)
 temperature                 = zeros(Float64, num_samples, num_years)
 global_mean_sea_level_rise  = zeros(Float64, num_samples, num_years)
-slr_antartic_icesheet       = zeros(Float64, num_samples, num_years)
+slr_antarctic_icesheet      = zeros(Float64, num_samples, num_years)
 slr_glaciers_small_ice_caps = zeros(Float64, num_samples, num_years)
-slr_greeland_icesheet       = zeros(Float64, num_samples, num_years)
+slr_greenland_icesheet      = zeros(Float64, num_samples, num_years)
 slr_landwater_storage       = zeros(Float64, num_samples, num_years)
 slr_thermal_expansion       = zeros(Float64, num_samples, num_years)
+ocean_heat                  = zeros(Float64, num_samples, num_years)
 
 # loop over each sample input and evaluate the model
 for i = 1:num_samples
@@ -95,7 +80,8 @@ for i = 1:num_samples
     t_peak = peak[i]
     γ_d = decline[i]
 
-    t, gtco2 = emissions_curve(γ_g=γ_g, t_peak=t_peak, γ_d=γ_d) # years and emissions
+    t, gtco2 = emissions_curve(historical_data, γ_g=γ_g, t_peak=t_peak, γ_d=γ_d) # years and emissions
+    gtco2 ./= 3.67 # divide by 3.67 to convert GtCO₂/yr to GtC/yr (SNEASY needs input of GtC/yr)
 
     # feed CO₂ emissions into SNEASY-BRICK
     update_param!(m, :ccm, :CO2_emissions, gtco2)
@@ -114,8 +100,25 @@ for i = 1:num_samples
     # ---------------------------------------------------- Create and Update SNEASY-BRICK Parameters --------------------------------------------------------- #
 
     # create parameters
+    lw_random_sample            = lw_storage[i,:]           # land water storage sample
+    σ_temperature               = calibrated_params[i,1]    # statistical noise parameter
+    σ_ocean_heat                = calibrated_params[i,2]    # statistical noise parameter
+    σ_glaciers                  = calibrated_params[i,3]    # statistical noise parameter
+    σ_greenland                 = calibrated_params[i,4]    # statistical noise parameter
+    σ_antarctic                 = calibrated_params[i,5]    # statistical noise parameter
+    σ_gmsl                      = calibrated_params[i,6]    # statistical noise parameter
+    σ²_white_noise_CO₂          = calibrated_params[i,7]    # statistical noise parameter (will not use)
+    ρ_temperature               = calibrated_params[i,8]    # statistical noise parameter
+    ρ_ocean_heat                = calibrated_params[i,9]    # statistical noise parameter
+    ρ_glaciers                  = calibrated_params[i,10]   # statistical noise parameter
+    ρ_greenland                 = calibrated_params[i,11]   # statistical noise parameter
+    ρ_antarctic                 = calibrated_params[i,12]   # statistical noise parameter
+    ρ_gmsl                      = calibrated_params[i,13]   # statistical noise parameter
+    α₀_CO₂                      = calibrated_params[i,14]   # statistical noise parameter (will not use)
     CO2_0                       = calibrated_params[i,15]
     N2O_0                       = calibrated_params[i,16]
+    temperature_0               = calibrated_params[i,17]   # statistical noise parameter (will not use)
+    ocean_heat_0                = calibrated_params[i,18]   # statistical noise parameter
     thermal_s0                  = calibrated_params[i,19]
     greenland_v0                = calibrated_params[i,20]
     glaciers_v0                 = calibrated_params[i,21]
@@ -149,7 +152,9 @@ for i = 1:num_samples
     antarctic_slope             = calibrated_params[i,49]
     antarctic_lambda            = calibrated_params[i,50]
     antarctic_temp_threshold    = calibrated_params[i,51]
-    # below this point are noise variables (used to create statistial noise to superimpose on results) ## TODO - INCLUDE LATER ##
+
+    # ----- Land Water Storage ----- #
+    update_param!(m, :landwater_storage, :lws_random_sample, lw_random_sample)
 
     # ----- Antarctic Ocean ----- #
     update_param!(m, :antarctic_ocean, :anto_α, anto_alpha)
@@ -201,30 +206,6 @@ for i = 1:num_samples
     # run SNEASY-BRICK
     run(m)
 
-    # --------------------------------------------- Incorporate feedback loop to calculate damages in DICE -------------------------------------------------- #
-
-"""    # get the array for temperature from SNEASY-BRICK
-    temp_1yr = getdataframe(m, :doeclim=>:temp)
-
-    # make array transferable to DICE by changing to 5-year timesteps
-    temp_5yr = DataFrame(time = zeros(Int64, 60), temp = zeros(Float64, 60))
-    j=1
-
-    for i in 1:nrow(temp_1yr)
-        if temp_1yr[i,:time] % 5 == 0 || temp_1yr[i,:time] % 10 == 0 # if year is an increment of 5
-            temp_5yr[j,:] = temp_1yr[i,:]
-            j+=1
-        end
-    end
-
-    temp_array = temp_5yr[:,:temp] # get array from df
-
-    # update temperature parameter in DICE with temperature results from SNEASY-BRICK
-    update_param!(DICE, :damages, :TATM, temp_array)
-
-    # run DICE
-    run(DICE)"""
-
     # --------------------------------------------------- Retrieve and store output for current run -------------------------------------------------------- #
 
     # parameter values to be saved 
@@ -233,45 +214,82 @@ for i = 1:num_samples
                  antarctic_precip0, antarctic_kappa, antarctic_flow0, antarctic_runoff_height0, antarctic_c, antarctic_bed_height0, antarctic_slope, antarctic_lambda, antarctic_temp_threshold]
 
     # write current sample to respective array
-    parameters[i,:]                     = param_vals
-    co2_emissions[i,:]                  = m[:ccm, :CO2_emissions]                               # total CO₂ emissions (GtCO₂/yr)
-    radiative_forcing[i,:]              = m[:radiativeforcing, :rf]                             # global radiative forcing (top of atmosphere) (W/m^2)
-    temperature[i,:]                    = m[:ccm, :temp]                                        # global mean temperature anomaly (K), relative to preindustrial
-    global_mean_sea_level_rise[i,:]     = m[:global_sea_level, :sea_level_rise]                 # total sea level rise from all components (m)
-    slr_antartic_icesheet[i,:]          = m[:global_sea_level, :slr_antartic_icesheet]          # sea level rise from the Antarctic ice sheet (m)
-    slr_glaciers_small_ice_caps[i,:]    = m[:global_sea_level, :slr_glaciers_small_ice_caps]    # sea level rise from glaciers and small ice caps (m)
-    slr_greeland_icesheet[i,:]          = m[:global_sea_level, :slr_greeland_icesheet]          # sea level rise from the Greenland ice sheet (m)
-    slr_landwater_storage[i,:]          = m[:global_sea_level, :slr_landwater_storage]          # sea level rise from landwater storage (m)
-    slr_thermal_expansion[i,:]          = m[:global_sea_level, :slr_thermal_expansion]          # sea level rise from thermal expansion (m)
+    parameters[i,:]                     = param_vals                                                # values for each parameter
+    co2_emissions[i,:]                  = m[:ccm, :CO2_emissions] .* 3.67                           # total CO₂ emissions (GtCO₂/yr)
+    radiative_forcing[i,:]              = m[:radiativeforcing, :rf]                                 # global radiative forcing (top of atmosphere) (W/m^2)
+    temperature[i,:]                    = m[:ccm, :temp]                                            # global mean temperature anomaly (K), relative to preindustrial
+    global_mean_sea_level_rise[i,:]     = m[:global_sea_level, :sea_level_rise]                     # total sea level rise from all components (m)
+    slr_antarctic_icesheet[i,:]         = m[:global_sea_level, :slr_antartic_icesheet]              # sea level rise from the Antarctic ice sheet (m)
+    slr_glaciers_small_ice_caps[i,:]    = m[:global_sea_level, :slr_glaciers_small_ice_caps]        # sea level rise from glaciers and small ice caps (m)
+    slr_greenland_icesheet[i,:]         = m[:global_sea_level, :slr_greeland_icesheet]              # sea level rise from the Greenland ice sheet (m)
+    slr_landwater_storage[i,:]          = m[:global_sea_level, :slr_landwater_storage]              # sea level rise from landwater storage (m)
+    slr_thermal_expansion[i,:]          = m[:global_sea_level, :slr_thermal_expansion]              # sea level rise from thermal expansion (m)
+    ocean_heat[i,:]                     = m[:doeclim, :heat_mixed] .+ m[:doeclim, :heat_interior]   # sum of ocean heat content anomaly in mixed layer and interior ocean (10²² J)
 
+    # -------------------------------------------------- Incorporate statistical noise for current run ----------------------------------------------------- #
+    
+    # calculate the statistical noise
+    noise_temperature   = simulate_ar1_noise(num_years, σ_temperature, ρ_temperature)
+    noise_ocean_heat    = simulate_ar1_noise(num_years, σ_ocean_heat, ρ_ocean_heat)
+    noise_glaciers      = simulate_ar1_noise(num_years, σ_glaciers, ρ_glaciers)
+    noise_greenland     = simulate_ar1_noise(num_years, σ_greenland, ρ_greenland)
+    noise_antarctic     = simulate_ar1_noise(num_years, σ_antarctic, ρ_antarctic)
+    noise_gmsl          = simulate_ar1_noise(num_years, σ_gmsl, ρ_gmsl)
+
+    # define indices for the start of statistical noise in 2010
+    noise_indices = findall((in)(2010:end_year), start_year:end_year)
+
+    # add noise to results from 2010 onwards (@view macro alters array in-place)
+    @view(slr_glaciers_small_ice_caps[i,:])[noise_indices] .+= noise_glaciers[noise_indices]
+    @view(slr_greenland_icesheet[i,:])[noise_indices]      .+= noise_greenland[noise_indices]
+    @view(slr_antarctic_icesheet[i,:])[noise_indices]      .+= noise_antarctic[noise_indices]
+    @view(global_mean_sea_level_rise[i,:])[noise_indices]  .+= noise_gmsl[noise_indices]
+    @view(temperature[i,:])[noise_indices]                 .+= noise_temperature[noise_indices]
+    @view(ocean_heat[i,:])[noise_indices]                  .+= noise_ocean_heat[noise_indices]
+
+    # define baseline indices to normalize results
+    temperature_norm_indices = findall((in)(1861:1880), start_year:end_year) # indices needed to normalize temperature anomalies relative to 1861-1880 mean
+    sealevel_norm_indices_1961_1990 = findall((in)(1961:1990), start_year:end_year) # indices needed to normalize sea level rise sources relative to the 1961-1990 mean
+    sealevel_norm_indices_1992_2001 = findall((in)(1992:2001), start_year:end_year) # indices needed to normalize sea level rise sources relative to the 1992-2001 mean
+    
+    # subtract off the mean so we have results relative to a baseline
+    @view(slr_glaciers_small_ice_caps[i,:]) .-= mean(slr_glaciers_small_ice_caps[i,:][sealevel_norm_indices_1961_1990])
+    @view(slr_greenland_icesheet[i,:])      .-= mean(slr_greenland_icesheet[i,:][sealevel_norm_indices_1992_2001])
+    @view(slr_antarctic_icesheet[i,:])      .-= mean(slr_antarctic_icesheet[i,:][sealevel_norm_indices_1992_2001])
+    @view(global_mean_sea_level_rise[i,:])  .-= mean(global_mean_sea_level_rise[i,:][sealevel_norm_indices_1961_1990])
+    @view(temperature[i,:])                 .-= mean(temperature[i,:][temperature_norm_indices])
+    @view(ocean_heat[i,:])                  .+= ocean_heat_0 # add starting value since there is no established baseline
+    
 end
 
 # save parameter names for df
 param_names = ["gamma_g", "t_peak", "gamma_d", "CO2_0", "N2O_0", names(calibrated_params)[19:end]...]
 
-# transform matrices to dataframes to improve interpretability (add run number and years)
+# transform matrices to dataframes to improve interpretability (add parameter names/years)
 parameters_df                   = DataFrame(parameters, param_names)
 co2_emissions_df                = DataFrame(co2_emissions, Symbol.([model_years...]))
 radiative_forcing_df            = DataFrame(radiative_forcing, Symbol.([model_years...]))
 temperature_df                  = DataFrame(temperature, Symbol.([model_years...]))
 global_mean_sea_level_rise_df   = DataFrame(global_mean_sea_level_rise, Symbol.([model_years...]))
-slr_antartic_icesheet_df        = DataFrame(slr_antartic_icesheet, Symbol.([model_years...]))
+slr_antarctic_icesheet_df       = DataFrame(slr_antarctic_icesheet, Symbol.([model_years...]))
 slr_glaciers_small_ice_caps_df  = DataFrame(slr_glaciers_small_ice_caps, Symbol.([model_years...]))
-slr_greeland_icesheet_df        = DataFrame(slr_greeland_icesheet, Symbol.([model_years...]))
+slr_greenland_icesheet_df       = DataFrame(slr_greenland_icesheet, Symbol.([model_years...]))
 slr_landwater_storage_df        = DataFrame(slr_landwater_storage, Symbol.([model_years...]))
 slr_thermal_expansion_df        = DataFrame(slr_thermal_expansion, Symbol.([model_years...]))
+ocean_heat_df                   = DataFrame(ocean_heat, Symbol.([model_years...]))
 
 # export output to .csv files
-save(joinpath(@__DIR__, "..", "results", "parameters.csv"), parameters_df)
-save(joinpath(@__DIR__, "..", "results", "emissions.csv"), co2_emissions_df)
-save(joinpath(@__DIR__, "..", "results", "radiative_forcing.csv"), radiative_forcing_df)
-save(joinpath(@__DIR__, "..", "results", "temperature.csv"), temperature_df)
-save(joinpath(@__DIR__, "..", "results", "gmslr.csv"), global_mean_sea_level_rise_df)
-save(joinpath(@__DIR__, "..", "results", "antarctic.csv"), slr_antartic_icesheet_df)
-save(joinpath(@__DIR__, "..", "results", "gsic.csv"), slr_glaciers_small_ice_caps_df)
-save(joinpath(@__DIR__, "..", "results", "greenland.csv"), slr_greeland_icesheet_df)
-save(joinpath(@__DIR__, "..", "results", "lw_storage.csv"), slr_landwater_storage_df)
-save(joinpath(@__DIR__, "..", "results", "thermal_expansion.csv"), slr_thermal_expansion_df)
+save(joinpath(@__DIR__, "..", "results", "$output_dir", "parameters.csv"), parameters_df)
+save(joinpath(@__DIR__, "..", "results", "$output_dir", "emissions.csv"), co2_emissions_df)
+save(joinpath(@__DIR__, "..", "results", "$output_dir", "radiative_forcing.csv"), radiative_forcing_df)
+save(joinpath(@__DIR__, "..", "results", "$output_dir", "temperature.csv"), temperature_df)
+save(joinpath(@__DIR__, "..", "results", "$output_dir", "gmslr.csv"), global_mean_sea_level_rise_df)
+save(joinpath(@__DIR__, "..", "results", "$output_dir", "antarctic.csv"), slr_antarctic_icesheet_df)
+save(joinpath(@__DIR__, "..", "results", "$output_dir", "gsic.csv"), slr_glaciers_small_ice_caps_df)
+save(joinpath(@__DIR__, "..", "results", "$output_dir", "greenland.csv"), slr_greenland_icesheet_df)
+save(joinpath(@__DIR__, "..", "results", "$output_dir", "lw_storage.csv"), slr_landwater_storage_df)
+save(joinpath(@__DIR__, "..", "results", "$output_dir", "thermal_expansion.csv"), slr_thermal_expansion_df)
+save(joinpath(@__DIR__, "..", "results", "$output_dir", "ocean_heat.csv"), ocean_heat_df)
 
 #=
 # create and view the netCDF file
