5606c6a1262225dd51468c2ec3c7d35b222a3825
vsrikrish <vs498@cornell.edu>
2023-04-28 15:42:30 -0400

    standardize model output

--- src/run_gsa.jl
+++ src/run_gsa.jl
@@ -38,7 +38,7 @@ function brick_run(M::Matrix{Float64}; yr=2100)
     # function returns a df of global mean sea level rise values
     gmslr = model_ensemble(M, start_year=start_year, end_year=end_year)
 
-    return gmslr # return GMSLR for the specified year (vector with length n_samples)
+    return (gmslr .- mean(gmslr)) / std(gmslr) # return centered GMSLR for the specified year (vector with length n_samples)
 end
 
 #initialize values
