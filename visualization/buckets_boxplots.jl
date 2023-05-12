d89cf8b060bae23776838ffa5d3f2140bdcd4565
darnellchl <ced227@cornell.edu>
2023-05-12 18:26:48 -0400

    separate large boxplot into two plots

--- visualization/buckets_boxplots.jl
+++ visualization/buckets_boxplots.jl
@@ -28,6 +28,76 @@ early_indices   = findall(parameters.t_peak .<= 2050)           # at or before 2
 middle_indices  = findall(2050 .< parameters.t_peak .< 2100)    # between 2050 and 2100
 late_indices    = findall(parameters.t_peak .>= 2100)           # at or after 2100
 
+# --------------------------------------------------------------------------------- #
+# -------------- Plot radiative forcing, temperature, and GMSLR ------------------- #
+# --------------------------------------------------------------------------------- #
+# YEARS AS COLUMNS
+let
+    all_outputs = []
+    outputs = [radiative_forcing, temperature, gmslr]
+    years = ["2100", "2200", "2300"]
+    # loop through outputs of rf, temp, and GMSLR
+    for (i, output) in enumerate(outputs)
+        all_years = []
+        # loop through years
+        for (j, year) in enumerate(years)
+            # boxplots for early, middle, and late peaking for current year
+            titles = ["Radiative Forcing: $year" "Temperature: $year" "Global Mean Sea Level Rise: $year"]
+            ylabels = ["Global Radiative Forcing (W/m²)", "Global Mean Temperature Anomaly (K)", "Global Mean Sea Level Anomaly (m)"]
+            xticks = (1:3, ["Early" "Middle" "Late"])
+            ylims = [(0,13), (0,11), (0,11)]
+            p1 = boxplot(xticks=xticks, ylim=ylims[i], title=titles[i], ylabel=ylabels[i])
+            boxplot!(output[early_indices,"$year"], color=:green) # early peaking
+            boxplot!(output[middle_indices,"$year"], color=:blue) # middle peaking
+            boxplot!(output[late_indices,"$year"], color=:purple) # late peaking
+            push!(all_years, p1) # add current year's plot to vector
+        end 
+        # combined plots for all outputs
+        p2 = plot(all_years..., layout=(1,3), legend=:false)
+        push!(all_outputs, p2)
+    end
+    # combined plots for all years
+    p3 = plot(all_outputs..., layout=(3,1), size=(1800,1200), bottom_margin=5mm, left_margin=10mm)
+    display(p3)
+    #savefig(p3, "/Users/ced227/Desktop/plots/boxplots.png")
+end
+
+#=
+# plot radiative forcing, temperature, and GMSLR -> YEARS AS ROWS
+let
+    all_years = []
+    years = ["2100", "2200", "2300"]
+    outputs = [radiative_forcing, temperature, gmslr]
+    #loop through years
+    for (i, year) in enumerate(years)
+        all_outputs = []
+        # loop through outputs of rf, temp, and GMSLR
+        for (j, output) in enumerate(outputs)
+            # boxplots for early, middle, and late peaking for current output
+            titles = ["Radiative Forcing: $year" "Temperature: $year" "Global Mean Sea Level Rise: $year"]
+            ylabels = ["Global Radiative Forcing (W/m²)", "Global Mean Temperature Anomaly (K)", "Global Mean Sea Level Anomaly (m)"]
+            xticks = (1:3, ["Early" "Middle" "Late"])
+            ylims = [(0,14), (0,11), (0,11)]
+            p1 = boxplot(xticks=xticks, ylim=ylims[j], title=titles[j], ylabel=ylabels[j])
+            boxplot!(output[early_indices,"$year"], color=:green) # early peaking
+            boxplot!(output[middle_indices,"$year"], color=:blue) # middle peaking
+            boxplot!(output[late_indices,"$year"], color=:purple) # late peaking
+            push!(all_outputs, p1) # add current output's plot to vector
+        end 
+        # combined plots for all outputs
+        p2 = plot(all_outputs..., layout=(1,3), legend=:false)
+        push!(all_years, p2)
+    end
+    # combined plots for all years
+    p3 = plot(all_years..., layout=(3,1), size=(1800,1200), bottom_margin=5mm, left_margin=10mm)
+    display(p3)
+    #savefig(p3, "/Users/ced227/Desktop/plots/boxplots.png")
+end
+=#
+
+# --------------------------------------------------------------------------------- #
+# ------------------------- Plot contributors to GMSLR ---------------------------- #
+# --------------------------------------------------------------------------------- #
 let
     all_years = []
     years = ["2100", "2200", "2300"]
@@ -37,31 +107,24 @@ let
         all_buckets = []
         # loop through buckets of early, middle, and late peaking
         for (j, bucket) in enumerate(buckets)
-            # boxplots for rf, temp, and GMSLR
-            xticks = (1:3, ["Radiative Forcing" "Temperature" "GMSLR"])
-            ylims = [(0,9), (0,11), (0,13)]
-            p1 = boxplot(radiative_forcing[bucket,"$year"], color=:purple, xticks=xticks, ylims=ylims[i])
-            boxplot!(temperature[bucket,"$year"], color=:grey)
-            boxplot!(gmslr[bucket,"$year"], color=:red)
             # boxplots for contributors to GMSLR
-            xticks = (1:5, ["Antarctic" "GSIC" "Greenland" "LW Storage" "Thermal Expansion"])
-            ylims = [(-0.1, 0.9), (-0.4, 3), (-0.6, 5)]
-            p2 = boxplot(antarctic[bucket,"$year"], xticks=xticks, ylims=ylims[i])
+            titles = ["Early Peaking: $year" "Middle Peaking: $year" "Late Peaking: $year"]
+            xticks = (1:5, ["Antarctic" "GSIC" "Greenland" "LW Storage" "TE"])
+            ylims = [(-0.1, 0.9), (-0.4, 4), (-0.6, 5.5)]
+            p1 = boxplot(antarctic[bucket,"$year"], xticks=xticks, ylims=ylims[i], 
+                        title=titles[j], ylabel="Global Mean Sea Level Anomaly (m)")
             boxplot!(gsic[bucket,"$year"])
             boxplot!(greenland[bucket,"$year"])
             boxplot!(lw_storage[bucket,"$year"])
             boxplot!(thermal_expansion[bucket,"$year"])
-            # combined plots of p1 and p2
-            titles = ["Early Peaking: $year" "Middle Peaking: $year" "Late Peaking: $year"]
-            p3 = plot(p1, p2, layout=(1,2), size=(700,400), xrotation=45, legend=:false, plot_title=titles[j])
-            push!(all_buckets, p3)
+            push!(all_buckets, p1) # add current bucket's plot to vector
         end 
         # combined plots for early, middle, and late peaking
-        p4 = plot(all_buckets..., layout=(1,3), size=(2100,400), legend=:false)
-        push!(all_years, p4)
+        p2 = plot(all_buckets..., layout=(1,3), legend=:false, bottom_margin=5mm, left_margin=10mm)
+        push!(all_years, p2)
     end
     # combined plots for all years
-    p5 = plot(all_years..., layout=(3,1), size=(2100,1200))
-    display(p5)
-    #savefig(p5, "/Users/ced227/Desktop/plots/boxplots.png")
+    p3 = plot(all_years..., layout=(3,1), size=(1800,1200))
+    display(p3)
+    #savefig(p3, "/Users/ced227/Desktop/plots/boxplots.png")
 end
