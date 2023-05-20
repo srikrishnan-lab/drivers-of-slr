d5b557150a37d85e44210db760117e539c345971
darnellchl <ced227@cornell.edu>
2023-05-19 23:01:57 -0400

    add "p =" to fix duplicating issue

--- visualization/gsa_plots.jl
+++ visualization/gsa_plots.jl
@@ -7,7 +7,7 @@ Pkg.instantiate()
 using Plots, Measures
 include("../src/functions.jl")
 
-output_dir = "10_000_samples"
+output_dir = "10_000_redo"
 
 # read in results
 first_order = DataFrame(load(joinpath(@__DIR__, "..", "results", "gsa_results", "$output_dir", "first_order.csv")))
@@ -24,13 +24,15 @@ all_plts = []
 years = ["2100", "2200", "2300"]
 
 for year in years
-    plt = scatter(first_order.parameter, first_order[:,"$year"], yerror = first_CI[:,"$year"], 
-            title = "$year", xlabel="Parameter", ylabel="Sensitivity", xticks=:all, legend=:false,
-            xrotation=45, markersize=3, tickfontsize=6, bottom_margin=7mm, left_margin=5mm)
+    plt = scatter(first_order.parameter, first_order[:,"$year"], yerror = first_CI[:,"$year"], legend=:false,
+            title = "$year", xlabel="Parameter", ylabel="Sensitivity", xticks=:all, xrotation=45, 
+            markersize=3, tickfontsize=6, bottom_margin=7mm, left_margin=5mm)
     push!(all_plts, plt)
 end
+#ylim=(-0.03,0.82), yticks=0:0.1:0.8,
 
 p = plot(all_plts..., plot_title="First Order Sensitivities", layout=(3,1), size=(900,1200))
+display(p)
 #savefig(p, "/Users/ced227/Desktop/plots/first_order.pdf")
 
 # -------------------------------------- Plot total order sensitivities --------------------------------------- #
@@ -38,11 +40,13 @@ all_plts = []
 years = ["2100", "2200", "2300"]
 
 for year in years
-    plt = scatter(total_order.parameter, total_order[:,"$year"], yerror = total_CI[:,"$year"], 
-            title = "$year", xlabel="Parameter", ylabel="Sensitivity", xticks=:all, legend=:false,
-            xrotation=45, markersize=3, tickfontsize=6, bottom_margin=7mm, left_margin=5mm)
+    plt = scatter(total_order.parameter, total_order[:,"$year"], yerror = total_CI[:,"$year"], legend=:false,
+            title = "$year", xlabel="Parameter", ylabel="Sensitivity", xticks=:all, xrotation=45,
+            markersize=3, tickfontsize=6, bottom_margin=7mm, left_margin=5mm)
     push!(all_plts, plt)
 end
+#ylim=(-0.02,0.62), yticks=0:0.1:0.6,
 
-plot(all_plts..., plot_title="Total Order Sensitivities", layout=(3,1), size=(900,1200))
+p = plot(all_plts..., plot_title="Total Order Sensitivities", layout=(3,1), size=(900,1200))
+display(p)
 #savefig(p, "/Users/ced227/Desktop/plots/total_order.pdf")
